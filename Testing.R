20c51e097ef12c6b2c3a30d2c161ea03d4934835
DEP <despinoza@gmx.com>
2023-10-14 07:37:59 -0500

    Initial commit of first program

---
+++ Testing.R
@@ -0,0 +1,6 @@
+print("Quest documento é stato creato en RStudio")
+print("Ce document est crée en RStudio")
+Print("This file was created within RStudio")
+print("And now lives on GitHub")
+print("Et il vives en Github")
+print("E addeso il vive en GitHub")
